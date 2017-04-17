bc079d61af7da0d921e91cf184ec19dbee0db2bc
Stefan George <noreply@sgeorge.de>
2017-04-17 19:36:47 +0200

    update to latest stable solidity version 4.10

--- contracts/solidity/Tokens/AbstractToken.sol
+++ contracts/solidity/Tokens/AbstractToken.sol
@@ -11,12 +11,6 @@ contract Token {
     function balanceOf(address owner) constant returns (uint256 balance);
     function allowance(address owner, address spender) constant returns (uint256 remaining);
 
-    // Token meta data
-    // Those are not abstract functions, because solc won't recognize generated getter functions for public variables as functions.
-    function name() constant returns (string) {}
-    function symbol() constant returns (string) {}
-    function decimals() constant returns (uint8) {}
-
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
