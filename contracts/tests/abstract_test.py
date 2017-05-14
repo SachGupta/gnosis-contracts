9b9e7c6b6b5001e9d833318a54e4d0ee16a833d7
Stefan George <noreply@sgeorge.de>
2017-05-14 10:21:29 +0200

    allow to pass hex/bytes addresses or contract instances for library dictionary

--- contracts/tests/abstract_test.py
+++ contracts/tests/abstract_test.py
@@ -6,6 +6,7 @@ from ethereum.tester import keys, accounts, TransactionFailed
 # standard libraries
 from unittest import TestCase
 from os import walk
+import string
 
 
 class AbstractTestContract(TestCase):
@@ -22,11 +23,26 @@ class AbstractTestContract(TestCase):
         self.s.block.number = self.HOMESTEAD_BLOCK
         t.gas_limit = 4712388
 
+    @staticmethod
+    def is_hex(s):
+        return all(c in string.hexdigits for c in s)
+
     def create_contract(self, path, params=None, libraries=None):
         abs_contract_path = '{}/{}'.format(ROOT_DIR, self.CONTRACT_DIR)
         sub_dirs = [x[0] for x in walk(abs_contract_path)]
         extra_args = ' '.join(['{}={}'.format(d.split('/')[-1], d) for d in sub_dirs])
         path = '{}/{}'.format(abs_contract_path, path)
+        if libraries:
+            for name, address in libraries.iteritems():
+                if type(address) == str:
+                    if self.is_hex(address):
+                        libraries[name] = address
+                    else:
+                        libraries[name] = address.encode('hex')
+                elif isinstance(address, t.ABIContract):
+                    libraries[name] = address.address.encode('hex')
+                else:
+                    raise ValueError
         return self.s.abi_contract(None,
                                    path=path,
                                    constructor_parameters=params,
