6d6720adfa9afc88e0926a79511d17ceededa37f
Stefan George <noreply@sgeorge.de>
2017-05-14 21:25:45 +0200

    added difficulty oracle factory contract

--- contracts/solidity/Oracles/DifficultyOracle.sol
+++ contracts/solidity/Oracles/DifficultyOracle.sol
@@ -2,64 +2,58 @@ pragma solidity 0.4.11;
 import "Oracles/AbstractOracle.sol";
 
 
-/// @title Difficulty oracle contract - On chain oracle to resolve difficulty events at given block.
+/// @title Difficulty oracle contract - Oracle to resolve difficulty events at given block.
 /// @author Stefan George - <stefan@gnosis.pm>
 contract DifficultyOracle is Oracle {
 
     /*
      *  Storage
      */
-    mapping (uint => uint) public difficultyResults;
+    uint blockNumber;
+    int outcome;
 
     /*
      *  Public functions
      */
-    /// @dev Sets difficulty as winning outcome for a specific block. Returns success.
-    /// @param blockNumber Block number, which has to be passed to set difficulty for block.
-    function setOutcome(uint blockNumber)
+    /// @dev Contract constructor validates and sets target block number.
+    /// @param _blockNumber Target block number.
+    function DifficultyOracle(uint _blockNumber)
         public
     {
-        if (block.number < blockNumber || difficultyResults[blockNumber] != 0)
+        if (_blockNumber < block.number)
+            // Block is in the past
+            revert();
+        blockNumber = _blockNumber;
+    }
+
+    /// @dev Sets difficulty as winning outcome for specified block.
+    function setOutcome()
+        public
+    {
+        if (block.number < blockNumber || outcome != 0)
             // Block number was not reached yet or it was set already
             revert();
-        difficultyResults[blockNumber] = block.difficulty;
+        outcome = int(block.difficulty);
     }
 
-    /// @dev Validates and registers event. Returns event identifier.
-    /// @param blockNumber Block number, which has to be passed to set difficulty for block.
-    /// @return Returns event identifier.
-    function registerEvent(uint blockNumber)
-        public
-        returns (bytes32 eventIdentifier)
-    {
-        if (blockNumber <= block.number)
-            // Block number was already reached
-            revert();
-        eventIdentifier = bytes32(blockNumber);
-    }
-
-    /// @dev Returns if winning outcome is set for given event.
-    /// @param eventIdentifier Event identifier.
+    /// @dev Returns if difficulty is set.
     /// @return Returns if outcome is set.
-    function isOutcomeSet(bytes32 eventIdentifier)
+    function isOutcomeSet()
         public
         constant
         returns (bool)
     {
-        uint blockNumber = uint(eventIdentifier);
-        // Difficulty will never be 0
-        return difficultyResults[blockNumber] > 0;
+        // Difficulty is always bigger than 0
+        return outcome > 0;
     }
 
     /// @dev Returns winning outcome for given event.
-    /// @param eventIdentifier Event identifier.
     /// @return Returns outcome.
-    function getOutcome(bytes32 eventIdentifier)
+    function getOutcome()
         public
         constant
         returns (int)
     {
-        uint blockNumber = uint(eventIdentifier);
-        return int(difficultyResults[blockNumber]);
+        return outcome;
     }
 }
---
+++ contracts/solidity/Oracles/DifficultyOracleFactory.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.4.11;
+import "Oracles/DifficultyOracle.sol";
+
+
+/// @title Difficulty oracle factory contract - Allows to create difficulty oracle contracts.
+/// @author Stefan George - <stefan@gnosis.pm>
+contract DifficultyOracleFactory {
+
+    /*
+     *  Storage
+     */
+    mapping (bytes32 => DifficultyOracle) public difficultyOracles;
+
+    /*
+     *  Public functions
+     */
+    /// @dev Creates a new difficulty oracle contract.
+    /// @param blockNumber Target block number.
+    /// @return Returns oracle contract.
+    function createDifficultyOracle(uint blockNumber)
+        public
+        returns (DifficultyOracle difficultyOracle)
+    {
+        bytes32 difficultyOracleHash = keccak256(blockNumber);
+        if (address(difficultyOracles[difficultyOracleHash]) != 0)
+            // Ultimate oracle exists already
+            revert();
+        difficultyOracle = new DifficultyOracle(blockNumber);
+        difficultyOracles[difficultyOracleHash] = difficultyOracle;
+    }
+}
